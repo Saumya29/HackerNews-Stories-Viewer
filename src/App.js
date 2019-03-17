f9d01d4edf991e7fdd59a7559b17bba2217433d8
Saumya29 <saumyatiwari.29@gmail.com>
2019-03-17 19:32:28 +0530

    View top stories of hackernews

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: [],
+    };
+  }
+
+  componentDidMount() {
+    const topStories = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+    const storyUrlBase = 'https://hacker-news.firebaseio.com/v0/item/';
+
+    fetch(topStories)
+    .then(data => data.json()) // .json returns a promise so chaining another .then
+    .then(data => data.map( id => { //we get an array of article id
+      const url = `${storyUrlBase}${id}.json`;
+      return fetch(url).then(data => data.json()) //.map returns an array of promises
+    }))
+    .then(promises => Promise.all(promises))
+    .then(stories => this.setState({stories}))
+  }
+
   render() {
+    let views = <div>Loading...</div>;
+    const {stories} = this.state;
+    if(stories && stories.length > 0) {
+      views = stories.map(story => (
+        <p key={story.id}>
+          <a href={story.url}>{story.title}</a> by {story.by}
+        </p>
+      ))
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>HackerNews Top Stories </h2>
+        {views}
       </div>
     );
   }
